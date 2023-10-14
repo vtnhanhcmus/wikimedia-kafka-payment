bc2d46f157693c017cd4205e8ec2d5d551f61281
Nhan Vo <thanhnhan.vo@kinesso.com>
2023-10-14 15:01:18 +0800

    config maven module

--- booking/src/main/java/com/wikimedia/booking/config/PaymentConsumerConfig.java
+++ booking/src/main/java/com/wikimedia/booking/config/PaymentConsumerConfig.java
@@ -4,7 +4,6 @@ import com.wikimedia.basedomain.PaymentRequest;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
