ca1155948057bf6e41761b2e5c1ad0c1eca398c2
Nhan Vo <vtnhanhcmus@gmail.com>
2023-10-20 17:39:03 +0800

    fix init data

---
+++ request/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email) VALUES ('david','david@gmail.com');
+INSERT INTO users (username, email) VALUES ('alice','alice@gmail.com');
+INSERT INTO users (username, email) VALUES ('nhan','nhan@gmail.com');
+INSERT INTO users (username, email) VALUES ('tien','tien@gmail.com');
+INSERT INTO users (username, email) VALUES ('tam','tam@gmail.com');
