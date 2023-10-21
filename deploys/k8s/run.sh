1bc3d0a4f6eaa1b0ee92a46ae02de7dc5de808f4
Nhan Vo <vtnhanhcmus@gmail.com>
2023-10-21 16:01:28 +0800

    add service payment for k8s

--- deploys/k8s/run.sh
+++ deploys/k8s/run.sh
@@ -2,6 +2,6 @@ kubectl apply -f 1_config-map.yml
 kubectl apply -f 2_wiki-postgres.yaml
 kubectl apply -f 3_wiki_zoo.yaml 
 kubectl apply -f 4_wiki-kafka.yml
-# kubectl apply -f 5_wiki_kafka-schema-registry.yaml 
-kubectl apply -f 6_wiki_request.yml
-kubectl apply -f 7_wiki_booking.yaml
+kubectl apply -f 5_wiki_request.yml
+kubectl apply -f 6_wiki_booking.yaml
+kubectl apply -f 7_wiki_payment.yaml
